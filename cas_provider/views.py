e91a4c09400a6bb40d8e6accb43da4d8fb4dc7e2
U-frozen\lreqc <lreqc@frozen.(none)>
2010-03-05 02:37:40 +0100

    Fixed adding ticket to query string, when service already constains a query string

--- cas_provider/views.py
+++ cas_provider/views.py
@@ -9,17 +9,27 @@ from cas_provider.forms import LoginForm
 from cas_provider.models import ServiceTicket, LoginTicket, auth_success_response
 from cas_provider.utils import create_service_ticket
 
+import urlparse, urllib
+
 __all__ = ['login', 'validate', 'service_validate', 'logout']
 
-def login(request, template_name='cas/login.html', success_redirect='/accounts/'):
+def _add_query_param(url, param, value):
+    parsed = urlparse.urlparse(url)
+    query = urlparse.parse_qs(parsed.query)
+    query[param] = [unicode(value)]
+    query = [ ((k, v) if len(v) > 1 else (k, v[0])) for k, v in query.iteritems() ]
+    parsed = urlparse.ParseResult(parsed.scheme, parsed.netloc,
+                                  parsed.path, parsed.params,
+                                  urllib.urlencode(query), parsed.fragment)
+    return parsed.geturl()
+
+
+def login(request, template_name = 'cas/login.html', success_redirect = '/accounts/'):
     service = request.GET.get('service', None)
     if request.user.is_authenticated():
         if service is not None:
             ticket = create_service_ticket(request.user, service)
-            if service.find('?') == -1:
-                return HttpResponseRedirect(service + '?ticket=' + ticket.ticket)
-            else:
-                return HttpResponseRedirect(service + '&ticket=' + ticket.ticket)
+            return HttpResponseRedirect(_add_query_param(service, 'ticket', ticket.ticket))
         else:
             return HttpResponseRedirect(success_redirect)
     errors = []
@@ -28,35 +38,36 @@ def login(request, template_name='cas/login.html', success_redirect='/accounts/'
         password = request.POST.get('password', None)
         service = request.POST.get('service', None)
         lt = request.POST.get('lt', None)
-        
+
         try:
-            login_ticket = LoginTicket.objects.get(ticket=lt)
+            login_ticket = LoginTicket.objects.get(ticket = lt)
         except:
             errors.append('Login ticket expired. Please try again.')
         else:
             login_ticket.delete()
-            user = authenticate(username=username, password=password)
+            user = authenticate(username = username, password = password)
             if user is not None:
                 if user.is_active:
                     auth_login(request, user)
                     if service is not None:
                         ticket = create_service_ticket(user, service)
-                        return HttpResponseRedirect(service + '?ticket=' + ticket.ticket)
+                        return HttpResponseRedirect(_add_query_param(service, 'ticket', ticket.ticket))
                     else:
+
                         return HttpResponseRedirect(success_redirect)
                 else:
                     errors.append('This account is disabled.')
             else:
                     errors.append('Incorrect username and/or password.')
     form = LoginForm(service)
-    return render_to_response(template_name, {'form': form, 'errors': errors}, context_instance=RequestContext(request))
-    
+    return render_to_response(template_name, {'form': form, 'errors': errors}, context_instance = RequestContext(request))
+
 def validate(request):
     service = request.GET.get('service', None)
     ticket_string = request.GET.get('ticket', None)
     if service is not None and ticket_string is not None:
         try:
-            ticket = ServiceTicket.objects.get(ticket=ticket_string)
+            ticket = ServiceTicket.objects.get(ticket = ticket_string)
             username = ticket.user.username
             ticket.delete()
             return HttpResponse("yes\n%s\n" % username)
@@ -72,20 +83,20 @@ def service_validate(request):
             <cas:authenticationFailure code="INVALID_REQUEST">
                 Not all required parameters were sent.
             </cas:authenticationFailure>
-        </cas:serviceResponse>''', mimetype='text/xml')
-    
+        </cas:serviceResponse>''', mimetype = 'text/xml')
+
     try:
-        ticket = ServiceTicket.objects.get(ticket=ticket_string)
+        ticket = ServiceTicket.objects.get(ticket = ticket_string)
         ticket.delete()
-        return HttpResponse(auth_success_response(ticket.user), mimetype='text/xml')
+        return HttpResponse(auth_success_response(ticket.user), mimetype = 'text/xml')
     except ServiceTicket.DoesNotExist:
         return HttpResponse('''<cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
             <cas:authenticationFailure code="INVALID_TICKET">
                 The provided ticket is invalid.
             </cas:authenticationFailure>
-        </cas:serviceResponse>''', mimetype='text/xml')
+        </cas:serviceResponse>''', mimetype = 'text/xml')
 
-def logout(request, template_name='cas/logout.html'):
+def logout(request, template_name = 'cas/logout.html'):
     url = request.GET.get('url', None)
     auth_logout(request)
-    return render_to_response(template_name, {'url': url}, context_instance=RequestContext(request))
+    return render_to_response(template_name, {'url': url}, context_instance = RequestContext(request))
